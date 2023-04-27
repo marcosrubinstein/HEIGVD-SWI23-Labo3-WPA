9fa63752b582aa0275f7b954b48ffb9c4b6d3772
Pascal Perrenoud <pascal@pband.ch>
2023-04-27 14:38:27 +0200

    First part avance

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -43,13 +43,14 @@ wpa=rdpcap("wpa_handshake.cap")
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
-ssid        = "SWI"
-APmac       = a2b_hex("cebcc8fdcab7")
-Clientmac   = a2b_hex("0013efd015bd")
 
+# TODO : Change const index to lookup for the 5 next vars
+ssid        = wpa[0][Dot11Beacon].network_stats()['ssid'] #"SWI"
+APmac       = a2b_hex(wpa[0][Dot11].addr2.replace(':','')) #a2b_hex("cebcc8fdcab7")
+Clientmac   = a2b_hex(wpa[1][Dot11].addr1.replace(':','')) #a2b_hex("0013efd015bd")
 # Authenticator and Supplicant Nonces
-ANonce      = a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce      = wpa[5][EAPOL].load[13:45]#a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+SNonce      = wpa[6][EAPOL].load[13:45]#a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
