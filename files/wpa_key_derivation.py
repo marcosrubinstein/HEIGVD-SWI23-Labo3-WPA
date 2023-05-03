f4a605820c01169082f5d7e4ba6ce6d81592b92e
Coney <35224441+MordoJ@users.noreply.github.com>
2023-05-03 15:19:06 +0200

    First part done

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -44,20 +44,22 @@ wpa=rdpcap("wpa_handshake.cap")
 passPhrase  = "actuelle"
 A           = "Pairwise key expansion" #this string is used in the pseudo-random function
 
-# TODO : Change const index to lookup for the 5 next vars
+# Récupération des informations de l'AP à partir du fichier pcap
 ssid        = wpa[0][Dot11Beacon].network_stats()['ssid'] #"SWI"
 APmac       = a2b_hex(wpa[0][Dot11].addr2.replace(':','')) #a2b_hex("cebcc8fdcab7")
 Clientmac   = a2b_hex(wpa[1][Dot11].addr1.replace(':','')) #a2b_hex("0013efd015bd")
+
 # Authenticator and Supplicant Nonces
-ANonce      = wpa[5][EAPOL].load[13:45]#a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-SNonce      = wpa[6][EAPOL].load[13:45]#a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
+ANonce      = wpa[5][EAPOL].load[13:45] #a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
+SNonce      = wpa[6][EAPOL].load[13:45] #a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
-mic_to_test = "36eef66540fa801ceee2fea9b7929b40"
+mic_to_test = b2a_hex(wpa[8][EAPOL].load[77:93]) #"36eef66540fa801ceee2fea9b7929b40"
 
 B           = min(APmac,Clientmac)+max(APmac,Clientmac)+min(ANonce,SNonce)+max(ANonce,SNonce) #used in pseudo-random function
 
+# La seule différence avec le payload du 4-way hand-shake est que le MIC n'est pas présent, il a été remplacé par des 0
 data        = a2b_hex("0103005f02030a0000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000") #cf "Quelques détails importants" dans la donnée
 
 print ("\n\nValues used to derivate keys")
