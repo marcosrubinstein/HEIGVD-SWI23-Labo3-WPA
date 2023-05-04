426afbc2d1268d5ea7ded71ec81014eccfa1cd51
Pascal Perrenoud <pascal@pband.ch>
2023-05-04 23:13:32 +0200

    Use helpers

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -28,6 +28,7 @@ from numpy import array_split
 from numpy import array
 import hmac
 import hashlib
+import helpers
 
 
 def customPRF512(key,A,B):
@@ -52,22 +53,22 @@ A           = "Pairwise key expansion"
 
 # Récupération des informations de l'AP à partir du fichier pcap
 # Previous value : "SWI"
-ssid        = wpa[0][Dot11Beacon].network_stats()['ssid']
+ssid        = helpers.find_ssid(wpa)
 # Previous value : a2b_hex("cebcc8fdcab7")
-APmac       = a2b_hex(wpa[0][Dot11].addr2.replace(':', ''))
+APmac       = a2b_hex(helpers.get_beacon_addr(wpa).replace(':', ''))
 # Previous value : a2b_hex("0013efd015bd")
-Clientmac   = a2b_hex(wpa[1][Dot11].addr1.replace(':', ''))
+Clientmac   = a2b_hex(helpers.get_client_addr(wpa).replace(':', ''))
 
 # Authenticator and Supplicant Nonces
 # Previous value : a2b_hex("90773b9a9661fee1f406e8989c912b45b029c652224e8b561417672ca7e0fd91")
-ANonce      = wpa[5][EAPOL].load[13:45]
+ANonce      = helpers.get_anonce(wpa)
 # Previous value : a2b_hex("7b3826876d14ff301aee7c1072b5e9091e21169841bce9ae8a3f24628f264577")
-SNonce      = wpa[6][EAPOL].load[13:45]
+SNonce      = helpers.get_snonce(wpa)
 
 # This is the MIC contained in the 4th frame of the 4-way handshake
 # When attacking WPA, we would compare it to our own MIC calculated using passphrases from a dictionary
 # Previous value : a2b_hex("36eef66540fa801ceee2fea9b7929b40")
-mic_to_test = wpa[8][EAPOL].load[77:93]
+mic_to_test = helpers.find_ssid(wpa)
 
 # used in pseudo-random function
 B           = min(APmac, Clientmac) + max(APmac, Clientmac) + min(ANonce, SNonce) + max(ANonce, SNonce)
