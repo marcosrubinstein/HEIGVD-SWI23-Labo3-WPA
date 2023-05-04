375e49b2fbbdb45ef8f284f13e1ae6e61c48236d
Pascal Perrenoud <pascal@pband.ch>
2023-05-04 22:03:36 +0200

    Correct python...

--- files/wpa_key_derivation.py
+++ files/wpa_key_derivation.py
@@ -35,11 +35,9 @@ def customPRF512(key,A,B):
     This function calculates the key expansion from the 256 bit PMK to the 512 bit PTK
     """
     blen = 64
-    i    = 0
     R    = b''
-    while i<=((blen*8+159)/160):
+    for i in range(4):
         hmacsha1 = hmac.new(key,A+str.encode(chr(0x00))+B+str.encode(chr(i)),hashlib.sha1)
-        i+=1
         R = R+hmacsha1.digest()
     return R[:blen]
 
